f288407e92d13a40f7affa4355a925a79e426d2a
Cristobal784 <cristobalvaldes@alu.unach.cl>
2026-08-18 12:12:45 -0400

    Actividad 2 - Componentes y Props

---
+++ src/components/Padre.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Hijo mensaje="Hola desde el padre" />
+    <Hijo2 mensaje="Que pasa malayas soy el hijo de en medio malditos madafakas" />
+    <Hijo3 mensaje="Buena petes soy el hijo menorete" />
+  </div>
+</template>
+
+<script>
+import Hijo from './Hijo.vue'
+import Hijo2 from './Hijo2.vue'
+import Hijo3 from './Hijo3.vue'
+
+export default {
+  components: {
+    Hijo,
+    Hijo2,
+    Hijo3
+  }
+}
+</script>

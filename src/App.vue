1d3b1683cba84dc7e5e93076f65aca5c27a3bdee
Cristobal784 <cristobalvaldes@alu.unach.cl>
2026-08-19 17:38:23 -0400

    Actividad 5 - Listas y condicionales con Vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
   <div id="app" style="padding: 20px;">
     
+    <h2>Actividad 1: Componente inicial</h2>
+    <HelloWorld msg="¡Hola desde la Actividad 1!" />
+    <hr style="margin: 40px 0;">
+
     <h2>Actividades 2 y 3: Componentes y Eventos</h2>
     <Padre />
-
     <hr style="margin: 40px 0;">
 
     <h2>Actividad 4: Formulario y v-model</h2>
     <Contacto />
+    <hr style="margin: 40px 0;">
+
+    <h2>Actividad 5: Lista de Tareas</h2>
+    <Tareas />
     
   </div>
 </template>
 
 <script>
+
+import HelloWorld from './components/HelloWorld.vue'
 import Padre from './components/Padre.vue'
 import Contacto from './components/Contacto.vue'
+import Tareas from './components/Tareas.vue' // <-- Nuevo import
 
 export default {
   components: {
+    HelloWorld,
     Padre,
-    Contacto
+    Contacto,
+    Tareas
   }
 }
 </script>
